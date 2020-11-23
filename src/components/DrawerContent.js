63297d67be58393cb1efc57791acdd144643aeb4
malsadi87 <mehmet.alsadi@gmail.com>
2020-11-23 11:35:23 +0000

    Update DrawerContent.js
    Fill drawer with profile information obtained from API

--- src/components/DrawerContent.js
+++ src/components/DrawerContent.js
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import {
     DrawerItem,
     DrawerContentScrollView
 } from '@react-navigation/drawer'
+
+import {PROFILE_END_POINT, MASTER_KEY_TOKEN} from '../model/end-points'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import {
     Avatar,
@@ -24,6 +27,40 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 const DrawerContent = (props) => {
 
     const {signOut} = useContext(AuthContext)
+    const [profile, setProfile] = useState({})
+
+    useEffect(()=>{
+
+        const getProfileData = async () => {
+              
+            let userId = null
+            let userToken = null
+            try {
+              userId = await AsyncStorage.getItem('userId')
+              userToken = await AsyncStorage.getItem('userToken')
+            } catch (e) {
+              // saving error
+              console.log(e)
+            }
+  
+          fetch(PROFILE_END_POINT,{
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json', 
+                Authorization: 'Bearer ' + userToken
+            }
+        })
+              .then((response) => response.json())
+              .then((responseData) => {
+                  //const filtered_results = responseData.rows.filter(row => row.chef.id == userId)
+                  console.log("My Profile " + JSON.stringify(responseData))
+                  setProfile(responseData)
+              }).done()
+        }
+            getProfileData()
+    },[])
+
     return (
         <View style={{ flex: 1 }}>
             <DrawerContentScrollView {...props} >
@@ -31,11 +68,11 @@ const DrawerContent = (props) => {
                     <View style={styles.userInfoSection}>
                         <View style={{ flexDirection: "row", marginTop: 15 }}>
                             <Avatar.Image
-                                source={{ uri: 'https://malsadi87.github.io/assets/img/mohammed.jpg' }}
+                                source={{ uri: profile.picture }}
                                 size={50} />
                             <View style={{ marginLeft: 20 }}>
-                                <Title style={styles.title}>Mohammed Alsadi</Title>
-                                <Caption style={styles.caption}>@malsadi</Caption>
+                                <Title style={styles.title}>{profile.name}</Title>
+                                <Caption style={styles.caption}>{profile.city}</Caption>
                             </View>
                         </View>
 
@@ -134,7 +171,9 @@ const DrawerContent = (props) => {
                         />
                     )}
                     label="Sign out"
-                    onPress={() => { signOut() }}
+                    onPress={() => { 
+                        setProfile({})
+                        signOut() }}
                 />
             </Drawer.Section>
         </View>
